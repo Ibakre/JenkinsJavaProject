157ebc9309f53f8c4afc09274992866a699c5ab9
Ibakre <109940019+Ibakre@users.noreply.github.com>
2022-08-02 16:44:21 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+    public static void main(String [] args)
+    {
+        System.out.print("Hello All!");
+        for(int i=0; i<10; i++)
+        {
+            System.out.print("Value of I is: " +i);
+        }
+    }
+}
